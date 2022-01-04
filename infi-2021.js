4468d90a5be369207a41965a6bda79ab17dd7734
jef <fockjef@gmail.com>
2022-01-03 21:06:03 -0600

    replace non-toy parts in one pass + minor cleanups

--- infi-2021.js
+++ infi-2021.js
@@ -17,79 +17,72 @@ AutoChassis: 31 IJzer
 Wiel: 71 Rubber, 19 IJzer
 Unobtanium: 7 IJzer, 29 Kryptonite`;
 
-let [ missing, ...toys] = input.split(/\n/),
-    subparts = {};
+let data = input.split(/\n/);
 
-// convert "missing" to a number
-missing = parseInt(missing);
-
-// convert "toys" to an object with a parts list for each toy
-toys = toys.reduce( ( toys, desc) => {
-    let [ toy, parts] = desc.split(/\s*:\s*/);
-    toy = toys[toy] = {};
+/*
+create toys object:
+{
+    toyName1 : { partName1: <number>, partName2, <number> ...},
+    toyName2 : { partName2: <number>, partName3, <number> ...},
+    ...
+}
+note: partNames may also be toyNames
+*/
+toys = data.slice(1).reduce( ( toys, desc) => {
+    let [ toyName, parts] = desc.split(/\s*:\s*/);
+    toys[toyName] = {};
     parts.split(/\s*,\s*/).forEach( item => {
         let [ cnt, part] = item.split(/\s+/);
-        toy[part] = +cnt;
+        toys[toyName][part] = +cnt;
     });
     return toys;
 }, {});
 
-let done = false;
-while( !done ){
-    done = true;
-    /* for each "toy" check if any of it's parts or also toys
-       if so then:
-       1. mark that part as a "subpart"
-       2. delete the part for the current toy's part list
-       3. add the part's subparts to the current toy (multiplied by the port's count) */
-    Object.keys(toys).forEach( toy => {
-        toy = toys[toy];
-        Object.keys(toy).forEach( part => {
-            if( part in toys ){
-                subparts[part] = true;
-                let cnt = toy[part];
-                delete toy[part];
-                part = toys[part];
-                Object.keys(part).forEach( subpart => {
-                    if( !(subpart in toy) ) toy[subpart] = 0;
-                    toy[subpart] += cnt * part[subpart];
-                });
-                done = false;
-            }
-        });
+/*
+replace any partName that is also a toyName with the corresponding
+number of parts from the associated toyName
+*/
+Object.keys(toys).forEach( toyName => {
+    let parts = toys[toyName];
+    Object.values(toys).forEach( partList => {
+        if( toyName in partList ){
+            let cnt = partList[toyName];
+            Object.keys(parts).forEach( p => partList[p] = (partList[p]||0) + cnt * parts[p]);
+            delete partList[toyName];
+            delete toys[toyName];
+        }
     });
-}
+});
 
-// remove non-toys from the toy list (ie: subparts)
-Object.keys(subparts).forEach( part => delete toys[part]);
-
-// reduce toys object to an array of the form [ [ toyName1, numParts1], [ toyName2, numParts2], ... ]
+/*
+convert toys object into an array of objects with each toys name and total number of parts
+sort the array by number of parts descending:
+[
+    { toyName: toyName1, numParts: <number> },
+    { toyName: toyName2, numParts: <number> },
+    ...
+]
+*/
 toys = Object.entries(toys)
-        .map( ([ toy, parts]) => [ toy, Object.values(parts).reduce( ( sum, x) => sum + x)])
-        .sort( ( a, b) => b[1] - a[1]); // sort by numParts descending
+    .map( ([ toyName, parts]) => ({
+        toyName,
+        numParts: Object.values(parts).reduce( ( sum, x) => sum + x)
+    }))
+    .sort( ( a, b) => b.numParts - a.numParts);
 
-// recursively test every linear combination of toys until criteria is matched
-function findCombos( toys, numToys, numParts){
-    if( toys.length == 0 ){
-        return [];
-    }
-    if( toys[0][1] * numToys == numParts ){
-        return [[ toys[0][0], numToys]];
-    }
+let part1 = toys[0].numParts;
+let part2 = (function findCombos( toys, numToys, numParts){
+    if( toys.length == 0 ) return null;
+    if( toys[0].numParts * numToys == numParts ) return [toys[0].toyName[0].repeat(numToys)];
     for( let i = 0; i < numToys; i++ ){
-        let nP = i * toys[0][1];
+        let nP = i * toys[0].numParts;
         if( nP < numParts ){
             let combo = findCombos( toys.slice(1), numToys - i, numParts - nP);
-            if( combo.length ){
-                return i == 0 ? combo : [ ...combo, [ toys[0][0], i]];
-            }
+            if( combo ) return i == 0 ? combo : [ ...combo, toys[0].toyName[0].repeat(i)];
         }
     }
-    return [];
-}
-
-let part1 = toys[0][1];
-let part2 = findCombos( toys, 20, missing).map( ([ toy, cnt]) => toy.charAt().repeat(cnt)).sort().join("");
+    return null;
+})( toys, 20, parseInt(data[0]) /* <-- number of missing parts */).sort().join("");
 
 console.log(`infi@nerd-pc ~/aoc2021 ▶ ${part1}`);
 console.log(`infi@nerd-pc ~/aoc2021 ▶ ${part2}`);
