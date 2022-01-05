0866d7022f1695c314594ce1a7b43f506ab7bf53
fockjef <fockjef@users.noreply.github.com>
2022-01-05 09:41:33 -0600

    whitespace

--- infi-2021.js
+++ infi-2021.js
@@ -1,6 +1,6 @@
 let input = "location" in globalThis && location.href.startsWith("https://infiaoc.azurewebsites.net/api/aoc/input/2021/")
-	? document.body.innerText.trim() // run directly from dev console of puzzle input url
-	: `492634 onderdelen missen
+    ? document.body.innerText.trim() // run directly from dev console of puzzle input url
+    : `492634 onderdelen missen
 Lightsaber: 61 Batterij, 19 Unobtanium
 HandheldComputer: 31 Batterij, 71 Printplaat, 47 Plastic
 ElectrischeRacebaan: 31 AutoChassis, 89 Printplaat, 59 Plastic, 31 Batterij, 3 Wiel
@@ -20,65 +20,65 @@ Unobtanium: 29 IJzer, 29 Kryptonite`;
 
 let numPartsUsed = parseInt(input),
     toys = getToys_v2(input).sort( ( a, b) => b.numParts - a.numParts),
-	part1 = toys[0].numParts,
-	part2 = findToyCombination( toys, 20, numPartsUsed).sort().join("");
+    part1 = toys[0].numParts,
+    part2 = findToyCombination( toys, 20, numPartsUsed).sort().join("");
 
 console.log(`infi@nerd-pc ~/aoc2021 ▶ ${part1}`);
 console.log(`infi@nerd-pc ~/aoc2021 ▶ ${part2}`);
 
 function getToys_v1(input){
-	/*
-	create toys object:
-	{
-		toyName1 : { partName1: <number>, partName2, <number> ...},
-		toyName2 : { partName2: <number>, partName3, <number> ...},
-		...
-	}
-	note: partNames may also be toyNames
-	*/
-	toys = input.split(/\n/).slice(1).reduce( ( toys, desc) => {
-		let [ toyName, parts] = desc.split(/\s*:\s*/);
-		toys[toyName] = {};
-		parts.split(/\s*,\s*/).forEach( item => {
-			let [ cnt, part] = item.split(/\s+/);
-			toys[toyName][part] = +cnt;
-		});
-		return toys;
-	}, {});
+    /*
+    create toys object:
+    {
+        toyName1 : { partName1: <number>, partName2, <number> ...},
+        toyName2 : { partName2: <number>, partName3, <number> ...},
+        ...
+    }
+    note: partNames may also be toyNames
+    */
+    toys = input.split(/\n/).slice(1).reduce( ( toys, desc) => {
+        let [ toyName, parts] = desc.split(/\s*:\s*/);
+        toys[toyName] = {};
+        parts.split(/\s*,\s*/).forEach( item => {
+            let [ cnt, part] = item.split(/\s+/);
+            toys[toyName][part] = +cnt;
+        });
+        return toys;
+    }, {});
 
-	/*
-	replace any partName that is also a toyName with the corresponding
-	number of parts from the associated toyName
-	*/
-	Object.keys(toys).forEach( toyName => {
-		let parts = toys[toyName];
-		Object.values(toys).forEach( partList => {
-			if( toyName in partList ){
-				let cnt = partList[toyName];
-				Object.keys(parts).forEach( p => partList[p] = (partList[p]||0) + cnt * parts[p]);
-				delete partList[toyName];
-				delete toys[toyName];
-			}
-		});
-	});
+    /*
+    replace any partName that is also a toyName with the corresponding
+    number of parts from the associated toyName
+    */
+    Object.keys(toys).forEach( toyName => {
+        let parts = toys[toyName];
+        Object.values(toys).forEach( partList => {
+            if( toyName in partList ){
+                let cnt = partList[toyName];
+                Object.keys(parts).forEach( p => partList[p] = (partList[p]||0) + cnt * parts[p]);
+                delete partList[toyName];
+                delete toys[toyName];
+            }
+        });
+    });
 
-	/*
-	convert toys object into an array of objects with each toys name and total number of parts
-	sort the array by number of parts descending:
-	[
-		{ toyName: toyName1, numParts: <number> },
-		{ toyName: toyName2, numParts: <number> },
-		...
-	]
-	*/
-	return Object.entries(toys)
-		.map( ([ toyName, parts]) => ({
-			toyName,
-			numParts: Object.values(parts).reduce( ( sum, x) => sum + x)
-		}));
+    /*
+    convert toys object into an array of objects with each toys name and total number of parts
+    sort the array by number of parts descending:
+    [
+        { toyName: toyName1, numParts: <number> },
+        { toyName: toyName2, numParts: <number> },
+        ...
+    ]
+    */
+    return Object.entries(toys)
+        .map( ([ toyName, parts]) => ({
+            toyName,
+            numParts: Object.values(parts).reduce( ( sum, x) => sum + x)
+        }));
 }
 
-/* 
+/*
 regex's _and_ eval? this might be against the geneva conventions
 converts partslist into numerical expression.
 example:
@@ -87,18 +87,18 @@ example:
 73*(17+89)+97, which is then "eval"-ed
 */
 function getToys_v2(input){
-	const reToyInfo = /^\w+: .*/mg;
-	input.match(reToyInfo).forEach( info => {
-		let [ toyName, parts] = info.split(":"),
-			reToyName = new RegExp( `(\\d+) ${toyName}\\b`, "g");
-		if( reToyName.test(input) ){
-			input = input.replace( reToyName, `$1*(${parts})`).replace( info, "");
-		}
-	});
-	return input.match(reToyInfo).map( info => ({
-		toyName: info.split(":")[0],
-		numParts: eval(info.split(":")[1].replace( /,/g, "+").replace( /[a-z ]/ig, ""))
-	}));
+    const reToyInfo = /^\w+: .*/mg;
+    input.match(reToyInfo).forEach( info => {
+        let [ toyName, parts] = info.split(":"),
+            reToyName = new RegExp( `(\\d+) ${toyName}\\b`, "g");
+        if( reToyName.test(input) ){
+            input = input.replace( reToyName, `$1*(${parts})`).replace( info, "");
+        }
+    });
+    return input.match(reToyInfo).map( info => ({
+        toyName: info.split(":")[0],
+        numParts: eval(info.split(":")[1].replace( /,/g, "+").replace( /[a-z ]/ig, ""))
+    }));
 }
 
 /*
